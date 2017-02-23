a04a24eba3795c37562273e758ba72fb5ef5749a
qinghaitvxq <598616202@qq.com>
2017-02-23 18:19:24 +0800

    add about.html

--- scripts/global.js
+++ scripts/global.js
@@ -54,9 +54,21 @@ function highlightPage() {
 }
 
 function moveElement(elementID,final_x,final_y,interval) {
+
+
     if(!document.getElementById) return false;
     if(!document.getElementById(elementID)) return false;
     var elem=document.getElementById(elementID);
+
+    if(elem.movement){
+        clearTimeout(ele.movement);
+    }
+    if(!elem.style.left){
+        elem.style.left="0px";
+    }
+    if(!elem.style.top){
+        elem.style.top="0px";
+    }
 
     var xpos=parseInt(elem.style.left);
     var ypos=parseInt(elem.style.top);
@@ -65,20 +77,105 @@ function moveElement(elementID,final_x,final_y,interval) {
         return true;
     }
     if(xpos<final_x){
-        xpos++;
+      var dist=Math.ceil((final_x-xpos)/10);
+      xpos=xpos+dist;
     }
     if(xpos>final_x){
-        xpos--;
+       var dist=Math.ceil((xpos-final_x)/10);
+       xpos=xpos-dist;
     }
     if(ypos<final_y){
-        ypos++;
+        var dist=Math.ceil((final_y-ypos)/10);
+        ypos=ypos+dist;
     }
     if(ypos>final_y){
-        ypos--;
+        var dist=Math.ceil((ypos-final_y)/10);
+        ypos=ypos-dist;
     }
     elem.style.left=xpos+"px";
     elem.style.top=ypos+"px";
     var repeat="moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
     movement=setTimeout(repeat,interval);
 }
+function prepareSlideshow() {
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("intro")) return false;
+
+    var intro=document.getElementById("intro");
+    var slideshow=document.createElement("div");
+    slideshow.setAttribute("id","slideshow");
+    var preview=document.createElement("img");
+    preview.setAttribute("src","images/slideshow.gif");
+    preview.setAttribute("alt","a glimpse of what awaits you");
+    preview.setAttribute("id","preview");
+    slideshow.appendChild(preview);
+
+    var frame=document.createElement('img');
+    frame.setAttribute('src','images/frame.gif');
+    frame.setAttribute('alt',"");
+    frame.setAttribute('id','frame');
+    slideshow.appendChild(frame);
+
+    insertAfter(slideshow,intro);
+
+    var links=document.getElementsByTagName("a");
+    var destination;
+    for(var i=0;i<links.length;i++){
+
+        links[i].onmouseover=function () {
+            destination=this.getAttribute("href");
+            if(destination.indexOf("index.html")!=-1){
+                moveElement("preview",0,0,5);
+            }
+            if(destination.indexOf("about.html")!=-1){
+                moveElement("preview",-150,0,5);
+            }
+            if(destination.indexOf("photos.html")!=-1){
+                moveElement("preview",-300,0,5);
+            }
+            if(destination.indexOf("live.html")!=-1){
+                moveElement("preview",-450,0,5);
+            }
+            if(destination.indexOf("contact.html")!=-1){
+                moveElement("preview",-600,0,5);
+            }
+
+        }
+    }
+}
+function showSection(id) {
+    var sections=document.getElementsByTagName('section');
+    for(var i=0;i<sections.length;i++){
+        if(sections[i].getAttribute('id')!=id){
+            sections[i].style.display='none';
+        }else{
+            sections[i].style.display="block";
+
+        }
+    }
+}
+function prepareInternalnav() {
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById) return false;
+    var articles=document.getElementsByTagName("article");
+    if(articles.length==0) return false;
+    var navs=articles[0].getElementsByTagName("nav");
+    if(navs.length==0) return false;
+    var nav=navs[0];
+    var links=nav.getElementsByTagName("a");
+    for(var i=0;i<links.length;i++){
+        var sectionId=links[i].getAttribute('href').split("#")[1];
+        if(!document.getElementById(sectionId)) continue;
+        document.getElementById(sectionId).style.display="none";
+        links[i].destination=sectionId;
+        links[i].onclick=function () {
+            showSection(this.destination);
+            return false;
+        }
+    }
+}
+
 addLoadEvent(highlightPage);
+addLoadEvent(prepareSlideshow);
+addLoadEvent(prepareInternalnav);
